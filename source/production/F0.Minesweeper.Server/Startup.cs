076c41d1cb0896e2e5678d8243faa38b5771cb46
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-10-21 18:05:30 +0200

    build: setup solution (#2)

---
+++ source/production/F0.Minesweeper.Server/Startup.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace F0.Minesweeper.Server
+{
+	internal class Startup
+	{
+		public Startup(IConfiguration configuration)
+			=> Configuration = configuration;
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddRazorPages();
+			services.AddServerSideBlazor();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+			}
+
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapBlazorHub();
+				endpoints.MapFallbackToPage("/_Host");
+			});
+		}
+	}
+}
