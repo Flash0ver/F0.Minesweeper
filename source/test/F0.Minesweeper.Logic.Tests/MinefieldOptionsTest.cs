41f0ffd2075e2885ab97e7c18be4f99c96e5f2b2
thomas-pemsel <thomas.pemsel91@gmail.com>
2020-10-27 23:28:54 +0100

    style(game-logic): remove unnecessary usings

--- source/test/F0.Minesweeper.Logic.Tests/MinefieldOptionsTest.cs
+++ source/test/F0.Minesweeper.Logic.Tests/MinefieldOptionsTest.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using F0.Minesweeper.Logic.Abstractions;
 using FluentAssertions;
 using Xunit;
