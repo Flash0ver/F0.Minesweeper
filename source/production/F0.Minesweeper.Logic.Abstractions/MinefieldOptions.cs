5609841b86046cfdef67c17323f150a27d04f01c
thomas-pemsel <thomas.pemsel91@gmail.com>
2020-10-27 19:43:54 +0100

    feat(game-logic): implement MinefieldOptions

--- source/production/F0.Minesweeper.Logic.Abstractions/MinefieldOptions.cs
+++ source/production/F0.Minesweeper.Logic.Abstractions/MinefieldOptions.cs
@@ -4,13 +4,13 @@ namespace F0.Minesweeper.Logic.Abstractions
 {
 	public sealed record MinefieldOptions(uint Width, uint Height, uint MineCount)
 	{
-		public static MinefieldOptions Easy()
-			=> throw new NotImplementedException();
+		public static MinefieldOptions Easy
+			=> new MinefieldOptions(10, 8, 10);
 
-		public static MinefieldOptions Medium()
-			=> throw new NotImplementedException();
+		public static MinefieldOptions Medium
+			=> new MinefieldOptions(18, 14, 40);
 
-		public static MinefieldOptions Hard()
-			=> throw new NotImplementedException();
+		public static MinefieldOptions Hard
+			=> new MinefieldOptions(24, 20, 99);
 	}
 }
