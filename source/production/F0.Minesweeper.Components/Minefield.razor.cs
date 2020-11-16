5f6530c1fde6ca0f7170436c94c4630cae3fa06a
Manuel Zelenka <manuel.zelenka@gmail.com>
2020-11-16 21:52:40 +0100

    refactor(components): fixes possible null reference in Minefield

--- source/production/F0.Minesweeper.Components/Minefield.razor.cs
+++ source/production/F0.Minesweeper.Components/Minefield.razor.cs
@@ -6,13 +6,18 @@ namespace F0.Minesweeper.Components
 	public partial class Minefield
 	{
 		[Parameter]
-		public MinefieldSize? Size { get; set; }
+		public MinefieldSize Size { get; set; }
 
 		private bool isValidSize;
 
+		public Minefield()
+		{
+			Size = new MinefieldSize(0, 0);
+		}
+
 		protected override void OnParametersSet()
 		{
-			isValidSize = Size is not null && Size.Height > 0 && Size.Width > 0;
+			isValidSize = Size.Height > 0 && Size.Width > 0;
 		}
 	}
 }
