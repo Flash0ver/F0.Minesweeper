9606080a3e57e935c317841feaf9ecef644f9c0c
Manuel Zelenka <manuel.zelenka@gmail.com>
2021-03-02 08:26:36 +0100

    refactor: adds layout and testing improvements
    Removes debugging background colors.
    Adds hr element to footer.
    Sets active class of NavLink via ActiveClass attribute.
    Adds automatic redirect to Game component.
    Adds dotnet watch run profile to launchSettings.
    Updates bunit nugets to preview 1.
    Adds snapshot tests for all layout components.

--- source/production/F0.Minesweeper.Server/Pages/Index.razor.cs
+++ source/production/F0.Minesweeper.Server/Pages/Index.razor.cs
@@ -1,6 +1,17 @@
+using Microsoft.AspNetCore.Components;
+
 namespace F0.Minesweeper.Server.Pages
 {
 	public partial class Index
 	{
+		[Inject]
+		private NavigationManager? NavigationManager { get; set; }
+
+		protected override void OnInitialized()
+		{
+			base.OnInitialized();
+
+			NavigationManager?.NavigateTo("game");
+		}
 	}
 }
