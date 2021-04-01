ba1629c8a98efb5912acac6aa4f48e9abd0728f3
Manuel Zelenka <manuel.zelenka@gmail.com>
2021-04-01 19:40:56 +0200

    fix(components): fixes automatic uncovering of cells (#60)
    auto uncover of adjacent cells updates ui
    automatic uncovering of cells in flagged and unsure status

---
+++ source/production/F0.Minesweeper.Components/Abstractions/Enums/CellInteractionType.cs
@@ -0,0 +1,12 @@
+namespace F0.Minesweeper.Components.Abstractions.Enums
+{
+	/// <summary>
+	/// Contains the ways how a cell can be interacted with.
+	/// </summary>
+	public enum CellInteractionType
+	{
+		LeftClick = 0,
+		RightClick = 1,
+		Automatic = 2
+	}
+}
