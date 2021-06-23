f0172894e7fd9873564fda8ea26e5273755b9cac
Manuel Zelenka <manuel.zelenka@gmail.com>
2021-06-23 20:09:05 +0200

    test: render cell component for game updater tests

--- source/test/F0.Minesweeper.Components.Tests/Logic/Game/GameUpdaterTests.cs
+++ source/test/F0.Minesweeper.Components.Tests/Logic/Game/GameUpdaterTests.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bunit;
 using F0.Minesweeper.Components.Abstractions;
 using F0.Minesweeper.Logic.Abstractions;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Xunit;
+
+using GameCell = F0.Minesweeper.Components.Pages.Game.Modules.Cell;
 
 namespace F0.Minesweeper.Components.Logic.Game
 {
@@ -65,16 +69,19 @@ namespace F0.Minesweeper.Components.Logic.Game
 				.And
 				.HaveCount(6);
 
-			IEnumerable<Pages.Game.Modules.Cell> GetCells(uint x, uint y)
+			IEnumerable<GameCell> GetCells(uint x, uint y)
 			{
+				using TestContext testContext = new();
+				testContext.Services.AddSingleton<ICellStatusManager>(new Mock<ICellStatusManager>(MockBehavior.Strict).Object);
+
 				for (uint i = 0; i < x; i++)
 				{
 					for (uint j = 0; j < y; j++)
 					{
-						yield return new()
-						{
-							Location = new Location(i, j)
-						};
+						Location location = new(i, j);
+						ComponentParameter parameter = ComponentParameterFactory.Parameter(nameof(GameCell.Location), location);
+						GameCell cellComponent = testContext.RenderComponent<GameCell>(parameter).Instance;
+						yield return cellComponent;
 					}
 				}
 			}
