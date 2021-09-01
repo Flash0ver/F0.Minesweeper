449ca341c2b9b5c74df1fc7ef412661cb88242b9
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2021-09-01 18:55:40 +0200

    refactor(footer): extract code block to code behind

---
+++ source/production/F0.Minesweeper.Components/Layout/Footer.razor.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Components;
+using NuGet.Versioning;
+
+namespace F0.Minesweeper.Components.Layout
+{
+	public partial class Footer
+	{
+		private static MarkupString Format(SemanticVersion version)
+		{
+			string preRelease = version.IsPrerelease ? $"-{version.Release}" : String.Empty;
+			string buildMetadata = version.HasMetadata ? $"+{Commit(version.Metadata)}" : String.Empty;
+
+			return (MarkupString)$"v{version.Major}.{version.Minor}.{version.Patch}{preRelease}{buildMetadata}";
+		}
+
+		private static string Commit(string commit) => $@"<a href=""https://github.com/Flash0ver/F0.Minesweeper/commit/{commit}"" target=""_blank"">{commit}</a>";
+	}
+}
