ee93f552651ada7c59ee6abc0bd55464bfa7b600
Manuel Zelenka <manuel.zelenka@gmail.com>
2021-11-24 18:34:41 +0100

    refactor: move translation logic to cellvisualisationmanager

--- source/production/F0.Minesweeper.Components/Logic/Cell/CellVisualisationManager.cs
+++ source/production/F0.Minesweeper.Components/Logic/Cell/CellVisualisationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using F0.Minesweeper.Components.Abstractions;
 using F0.Minesweeper.Components.Abstractions.Enums;
@@ -6,38 +7,53 @@ namespace F0.Minesweeper.Components.Logic.Cell
 {
 	internal class CellVisualisationManager : ICellVisualisationManager
 	{
-		private static readonly Dictionary<CellStatusType, CellStatusTranslation> translations = InitializeTranslations();
+		private static readonly Dictionary<CellStatusType, (char? content, string cssClass)> visualisationTemplates = InitializeTranslations();
 		public CellVisualisation GetVisualisation(CellStatusType cellStatus) => GetVisualisation(cellStatus, null);
 		public CellVisualisation GetVisualisation(CellStatusType cellStatus, byte? adjacentMineCount)
 		{
-			CellStatusTranslation translation = GetTranslationOrDefault(cellStatus);
+			(char? content, string cssClass) template = GetVisualisationTemplateOrDefault(cellStatus);
 
-			char content = translation.GetDisplayValue(adjacentMineCount);
-			string cssClass = translation.GetCssClass(adjacentMineCount);
+			string cssClass = string.Format(template.cssClass, adjacentMineCount);
+
+			if(template.content is null && adjacentMineCount is null)
+			{
+				throw new ArgumentNullException(nameof(adjacentMineCount));
+			}
+
+			char content = template.content ?? GetAdjacentMineCountContent(adjacentMineCount!.Value);
 
 			return new(content, cssClass);
 		}
 
-		private static CellStatusTranslation GetTranslationOrDefault(CellStatusType status)
+		private static char GetAdjacentMineCountContent(byte adjacentMineCount)
 		{
-			if (translations.TryGetValue(status, out CellStatusTranslation? translation))
+			return adjacentMineCount switch
+			{
+				0 => ' ',
+				_ => adjacentMineCount.ToString()[0]
+			};
+		}
+
+		private static (char? content, string cssClass) GetVisualisationTemplateOrDefault(CellStatusType status)
+		{
+			if (visualisationTemplates.TryGetValue(status, out (char?, string) translation))
 			{
 				return translation;
 			}
 
-			return new CellStatusTranslation('!', "f0-cell f0-cell-undefinedstatus");
+			return new ('!', "f0-cell f0-cell-undefinedstatus");
 		}
 
-		private static Dictionary<CellStatusType, CellStatusTranslation> InitializeTranslations()
+		private static Dictionary<CellStatusType, (char?, string)> InitializeTranslations()
 		{
 			const string cellLayout = "f0-cell";
 
-			return new Dictionary<CellStatusType, CellStatusTranslation>
+			return new Dictionary<CellStatusType, (char?, string)>
 				{
 					{ CellStatusType.Covered, new (' ', $"{cellLayout} f0-cell-covered") },
 					{ CellStatusType.Flagged, new ('⚐', $"{cellLayout} f0-cell-covered") },
 					{ CellStatusType.FlaggedWrong, new ('⚐', $"{cellLayout} f0-cell-flagged-wrong") },
-					{ CellStatusType.Uncovered, new ($"{cellLayout} f0-cell-uncovered f0-cell-{{0}}-adjacent-mines") },
+					{ CellStatusType.Uncovered, new (null, $"{cellLayout} f0-cell-uncovered f0-cell-{{0}}-adjacent-mines") },
 					{ CellStatusType.Unsure, new ('?', $"{cellLayout} f0-cell-covered") },
 					{ CellStatusType.Mine, new ('☢', $"{cellLayout} f0-cell-uncovered") },
 					{ CellStatusType.MineExploded, new ('☢', $"{cellLayout} f0-cell-mine-exploded") }
---
+++ source/test/F0.Minesweeper.Components.Tests/Logic/Cell/CellVisualisationManagerTests.cs
@@ -0,0 +1,66 @@
+using System;
+using F0.Minesweeper.Components.Abstractions.Enums;
+using F0.Minesweeper.Components.Logic.Cell;
+using FluentAssertions;
+using Xunit;
+
+namespace F0.Minesweeper.Components.Tests.Logic.Cell
+{
+	public class CellVisualisationManagerTests
+	{
+		[Fact]
+		public void GetVisualisation_UncoveredWithoutAdjacentMineCount_Throws()
+		{
+			// Arrange
+			CellVisualisationManager instanceUnderTest = new();
+
+			// Act && Assert
+			Func<CellVisualisation> methodUnderTest = () => instanceUnderTest.GetVisualisation(CellStatusType.Uncovered);
+			methodUnderTest.Should().Throw<ArgumentNullException>();
+		}
+
+		[Theory]
+		[MemberData(nameof(CellVisualisationManagerTestData.TestData), MemberType = typeof(CellVisualisationManagerTestData))]
+		public void GetVisualisation_CorrectParameters_ReturnsCorrectVisualisation(VisualisationData visualisationData)
+		{
+			// Arrange
+			CellVisualisationManager instanceUnderTest = new();
+
+			// Act 
+			CellVisualisation visualisation = instanceUnderTest.GetVisualisation(visualisationData.CellStatusType, visualisationData.AdjacentMineCount);
+
+			// Assert
+			visualisation.Content.Should().Be(visualisationData.ExpectedContent);
+			visualisation.CssClass.Should().NotBeNull();
+		}
+
+		public class CellVisualisationManagerTestData
+		{
+			public static TheoryData<VisualisationData> TestData => GenerateTestData();
+		}
+
+		private static TheoryData<VisualisationData> GenerateTestData()
+		{
+			TheoryData<VisualisationData> resultData = new();
+
+			resultData.Add(new(CellStatusType.Uncovered, 0, ' '));
+			resultData.Add(new(CellStatusType.Uncovered, 1, '1'));
+			resultData.Add(new(CellStatusType.Uncovered, 2, '2'));
+			resultData.Add(new(CellStatusType.Uncovered, 3, '3'));
+			resultData.Add(new(CellStatusType.Uncovered, 4, '4'));
+			resultData.Add(new(CellStatusType.Uncovered, 5, '5'));
+			resultData.Add(new(CellStatusType.Uncovered, 6, '6'));
+			resultData.Add(new(CellStatusType.Uncovered, 7, '7'));
+			resultData.Add(new(CellStatusType.Uncovered, 8, '8'));
+			resultData.Add(new(CellStatusType.Covered, null, ' '));
+			resultData.Add(new(CellStatusType.Flagged, null, '⚐'));
+			resultData.Add(new(CellStatusType.FlaggedWrong, null, '⚐'));
+			resultData.Add(new(CellStatusType.Unsure, null, '?'));
+			resultData.Add(new(CellStatusType.Mine, null, '☢'));
+			resultData.Add(new(CellStatusType.MineExploded, null, '☢'));
+			return resultData;
+		}
+
+		public record VisualisationData(CellStatusType CellStatusType, byte? AdjacentMineCount, char ExpectedContent);
+	}
+}
