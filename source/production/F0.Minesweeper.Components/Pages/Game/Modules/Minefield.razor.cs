e3a2b961e357ded04a16f4ff3f37a58cfb14436d
thomas-pemsel <thomas.pemsel91@gmail.com>
2021-11-24 18:54:01 +0100

    feat(logic): set standard shuffler to FisherYates
    fix .globalconfig file ending

--- source/production/F0.Minesweeper.Components/Pages/Game/Modules/Minefield.razor.cs
+++ source/production/F0.Minesweeper.Components/Pages/Game/Modules/Minefield.razor.cs
@@ -31,7 +31,7 @@ namespace F0.Minesweeper.Components.Pages.Game.Modules
 
 		public Minefield()
 		{
-			Options = new MinefieldOptions(0, 0, 0, MinefieldFirstUncoverBehavior.MayYieldMine, LocationShuffler.GloballyUniqueIdentifier);
+			Options = new MinefieldOptions(0, 0, 0, MinefieldFirstUncoverBehavior.MayYieldMine, LocationShuffler.FisherYates);
 			cells = new List<Cell>();
 		}
 
