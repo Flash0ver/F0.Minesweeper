9da10554bacd47c9419410816f91470a5a3dfc7e
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2022-01-07 01:58:01 +0100

    perf: fix Benchmarks

--- source/performance/F0.Minesweeper.Logic.Benchmarks/LocationShuffler/LocationShufflerBenchmarks.cs
+++ source/performance/F0.Minesweeper.Logic.Benchmarks/LocationShuffler/LocationShufflerBenchmarks.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using F0.Minesweeper.Logic.Abstractions;
 using F0.Minesweeper.Logic.LocationShuffler;
 
@@ -5,49 +6,64 @@ namespace F0.Minesweeper.Logic.Benchmarks.LocationShuffler
 {
 	public class LocationShufflerBenchmarks
 	{
-		[ParamsSource(nameof(Parameters))]
-		public MinefieldOptions parameters = null!;
-
 		private readonly ILocationShuffler globallyUniqueIdentifier = new GuidLocationShuffler();
 		private readonly ILocationShuffler randomOrder = new RandomOrderLocationShuffler();
 		private readonly ILocationShuffler fisherYates = new FisherYatesLocationShuffler();
 
-		private IEnumerable<Location> minefield = Array.Empty<Location>();
+		private IEnumerable<Location> minefield = null!;
+
+		[ParamsSource(nameof(Parameters))]
+		public Param Parameter { get; set; } = null!;
 
 		[GlobalSetup]
 		public void GlobalSetup()
 		{
-			List<Location> locations = new((int)parameters.Width * (int)parameters.Height);
+			uint size = Parameter.Width * Parameter.Height;
+			List<Location> locations = new((int)size);
 
-			for (uint width = 0; width < parameters.Width; width++)
+			for (uint width = 0; width < Parameter.Width; width++)
 			{
-				for (uint height = 0; height < parameters.Height; height++)
+				for (uint height = 0; height < Parameter.Height; height++)
 				{
 					locations.Add(new Location(width, height));
 				}
 			}
 
+			Debug.Assert(locations.Count == size);
 			minefield = locations;
 		}
 
 		[Benchmark]
 		public IReadOnlyCollection<Location> GloballyUniqueIdentifier()
-			=> globallyUniqueIdentifier.ShuffleAndTake(minefield, (int)parameters.MineCount);
+			=> globallyUniqueIdentifier.ShuffleAndTake(minefield, (int)Parameter.MineCount);
 
 		[Benchmark]
 		public IReadOnlyCollection<Location> RandomOrder()
-			=> randomOrder.ShuffleAndTake(minefield, (int)parameters.MineCount);
+			=> randomOrder.ShuffleAndTake(minefield, (int)Parameter.MineCount);
 
 		[Benchmark]
 		public IReadOnlyCollection<Location> FisherYates()
-			=> fisherYates.ShuffleAndTake(minefield, (int)parameters.MineCount);
+			=> fisherYates.ShuffleAndTake(minefield, (int)Parameter.MineCount);
 
-		public static IEnumerable<MinefieldOptions> Parameters()
-			=> new[]
+		public static IEnumerable<Param> Parameters()
+		{
+			MinefieldOptions[] collection = new[]
 			{
 				MinefieldOptions.Easy,
 				MinefieldOptions.Medium,
 				MinefieldOptions.Hard,
 			};
+
+			foreach (MinefieldOptions item in collection)
+			{
+				yield return new Param(item.Width, item.Height, item.MineCount);
+			}
+		}
+
+		public record class Param(uint Width, uint Height, uint MineCount)
+		{
+			public override string ToString()
+				=> $"X:{Width:D2}, Y:{Height:D2}, Mines:{MineCount:D2}";
+		}
 	}
 }
--- source/performance/F0.Minesweeper.Logic.Benchmarks/Program.cs
+++ source/performance/F0.Minesweeper.Logic.Benchmarks/Program.cs
@@ -2,25 +2,19 @@ using System.Diagnostics;
 using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
-using BenchmarkDotNet.Environments;
-using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
-using BenchmarkDotNet.Validators;
 
 IConfig config = Debugger.IsAttached
 	? CreateDebugConfiguration()
 	: CreateBenchmarkConfiguration();
 
-_ = BenchmarkRunner.Run(typeof(Program).Assembly, config, args);
+_ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
 
 static IConfig CreateBenchmarkConfiguration()
 	=> DefaultConfig.Instance
-		.AddJob(Job.InProcess.WithRuntime(CoreRuntime.Core60))
-		.AddColumn(StatisticColumn.Min, StatisticColumn.Max, StatisticColumn.Median)
-		.AddDiagnoser(MemoryDiagnoser.Default)
-		.AddExporter(DefaultExporters.AsciiDoc)
-		.AddValidator(ExecutionValidator.FailOnError);
+		.WithOptions(ConfigOptions.StopOnFirstError)
+		.AddColumn(StatisticColumn.Min, StatisticColumn.Max, StatisticColumn.Median, RankColumn.Arabic)
+		.AddDiagnoser(MemoryDiagnoser.Default);
 
 static IConfig CreateDebugConfiguration()
 	=> new DebugInProcessConfig();
